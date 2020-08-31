f95cb7b7d426955b772ebb0b4261c9430635dd9c
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-08-31 15:29:26 -0400

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
+import TodoList from './components/TodoList'
 
-class App extends React.Component {
+const todoInitial = [];
+
+class App extends Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super()
+    this.state = {
+      todo: todoInitial,
+    }
+  }
+  // submit button
+  addItem = (item) => {
+    const toBeAdded = {
+      name: item,
+      id: new Date(),
+      done: false
+    }
+    this.setState({...this.state.todo, toBeAdded})
+  }
+
+  // clear button
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList todo={this.state.todo}/>
       </div>
     );
   }
