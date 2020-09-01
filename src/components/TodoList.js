5add7cf30bc543128624d213aac6d5e4f46c5a63
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-08-31 22:16:31 -0400

    added search functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,10 +6,32 @@ import Todo from './Todo'
 
 class TodoList extends Component {
 
+    constructor() {
+        super()
+        this.state = {
+          searchFilter: ''
+        }
+      }
+    changeSearchFilter = (event) => {
+        this.setState({searchFilter: event.target.value})
+    }
+
+
     render(){
+        const filter = this.props.todo.filter( results => {
+            return results.task.indexOf(this.state.searchFilter) !== -1
+        })
         return (
             <div>
-                {this.props.todo.map(item => (
+                <label>Search Todo List: </label>
+                <input
+                    type='text'
+                    name='searchFilter'
+                    placeholder='Filter'
+                    value={this.state.searchFilter}
+                    onChange={this.changeSearchFilter}>    
+                    </input>
+                {filter.map(item => (
                     <Todo key={item.id} todoItem={item} toggleCompleted={this.props.toggleCompleted}/>
                 ))}
             </div>
